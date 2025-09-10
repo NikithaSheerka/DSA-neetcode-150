3a09bd41ac75b5e201b1beb494a3784f57abf63d
NikithaSheerka <nikithasirka06@gmail.com>
2025-09-10 16:14:30 -0700

    Solved 1. Two Sum

---
+++ 1. Two Sum/1-Two-Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+        hashMap = {}
+
+        for i in range(len(nums)):
+            if nums[i] in hashMap:
+                return [hashMap[nums[i]], i]
+            else:
+                hashMap[target - nums[i]] = i
